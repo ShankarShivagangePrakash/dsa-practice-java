06c36b3fb3b73333cfb1507ea2ec1ae333be2b7b
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-12-11 08:59:42 +0530

    Morris Inorder Traversal of a Binary Tree:

---
+++ src/takeu-forward/tree/binary_tree/hard/MorrisInorderTraversalBinaryTree.java
@@ -0,0 +1,71 @@
+package tree.binary_tree.hard;
+
+import tree.binary_tree.traversal.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Problem:
+    https://takeuforward.org/data-structure/morris-inorder-traversal-of-a-binary-tree/
+    https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ */
+public class MorrisInorderTraversalBinaryTree {
+
+    /*
+    Approach: Explained in detail, in notes, check
+
+        Time Complexity: O(2n)
+            The time complexity is linear,
+                as each node is visited at most twice (once for establishing the temporary link and once for reverting it).
+                In each step, we perform constant-time operations, such as moving to the left or right child and updating pointers.
+
+        Space Complexity: O(1)
+             no additional space used.
+     */
+    public List<Integer> inOrderTraversal(TreeNode root){
+        // List to store the inorder traversal result
+        ArrayList<Integer> result = new ArrayList<>();
+        if(root == null)
+            return result;
+
+        // Pointer to the current node, starting from the root. This will be used for traversal
+        TreeNode cur = root;
+
+        while(cur != null){
+            // Case 1: If the current node's left child is NULL
+            if(cur.left == null){
+                // since there is no left child, we can add current node to inorder list and move right
+                result.add(cur.val);
+                cur = cur.right;
+            }
+            else{
+                /* case-2:
+                 There is a left subtree, and the right-most child of this left subtree is pointing to null.
+                 Set the right-most child of the left subtree to point to the current node.
+                 Move to the left child of the current node.*/
+                TreeNode prev = cur.left;
+                while(prev.right != null && prev.right != cur){
+                    prev = prev.right;
+                }
+                // if blcok belongs to case 2
+                if(prev.right == null){
+                    prev.right = cur;
+                    cur = cur.left;
+                }
+                /* Case 3:
+                There is a left subtree, and the right-most child of this left subtree is already pointing to the current node.
+				 Print the value of the current node.
+				 Revert the temporary link (set it back to null).
+                 Move to the right child of the current node.
+                 else means case 3*/
+                else{
+                    prev.right = null;
+                    result.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
+        return result;
+    }
+}
